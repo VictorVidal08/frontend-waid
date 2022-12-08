210e9e00804bd24aa37f7dbd23287bbabf97c778
VictorVidal08 <victorvidal08@hotmail.com>
2022-12-08 12:27:46 -0300

    feat: adding login page implementation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
+import { ILogin } from "../interfaces/ILogin";
+import { login } from '../services/Requests';
 
 export default function Login() {
+    const [formLogin, setFormLogin] = useState({ email:'', password:'' });
+    const [enable, setEnable] = useState(true);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const MIN_PASSWORD_LENGTH = 5;
+        const regexEmail = /\S+@\S+\.\S+/;
+        const validateEmail = regexEmail.test(formLogin.email);
+        if (formLogin.password.length >= MIN_PASSWORD_LENGTH && validateEmail) {
+            setEnable(false);
+        }
+        if (formLogin.password.length <= MIN_PASSWORD_LENGTH || !validateEmail) {
+            setEnable(true);
+        }
+    }, [formLogin]);
+
+    const handleLogin = async () => {
+        try {
+            const user = await login(formLogin as ILogin);
+            const userData = user.data;
+            localStorage.setItem('user', JSON.stringify(userData));
+            navigate('/social-posts')
+        } catch (err) {
+            console.log(err);
+        }
+    };    
+
     return (
         <div>
         <h1>Login</h1>
@@ -12,8 +40,10 @@ export default function Login() {
                 Email:
                 <input
                 placeholder="Email"
-                type="text" 
+                type="email" 
                 name="email"
+                value={ formLogin.email }
+                onChange={ ({ target }) => setFormLogin({ ...formLogin, email: target.value }) }
                 />
             </label>
             <label>
@@ -22,11 +52,14 @@ export default function Login() {
                 placeholder="******"
                 type="password"
                 name="password"
+                value={ formLogin.password }
+                onChange={ ({ target }) => setFormLogin({ ...formLogin, password: target.value }) }
                 />
             </label>
             <button
+            disabled={ enable }
             type="button"
-            onClick={ () => navigate('/social-posts')}
+            onClick={ handleLogin }
             >
                 Login
             </button>
--- src/services/Requests.ts
+++ src/services/Requests.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { IRegister } from '../interfaces/IRegister';
+import { ILogin } from '../interfaces/ILogin';
 
 //  Local: http://localhost:3001/user
 //  Production link: https://backend-waid-production.up.railway.app/register
@@ -8,3 +9,11 @@ export async function register(userData: IRegister) {
       { email: userData.email, password: userData.password, userName: userData.userName, image: userData.image });
     return response;
   }
+
+//  Local: http://localhost:3001/login
+//  Production link: https://backend-waid-production.up.railway.app/login
+export async function login(userData: ILogin) {
+    const response = await axios.post('http://localhost:3001/login', 
+      { email: userData.email, password: userData.password });
+    return response;
+}
