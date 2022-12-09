232bb08cd0ced9f5d92d6a9407b496d15afcd78b
VictorVidal08 <victorvidal08@hotmail.com>
2022-12-08 21:45:34 -0300

    feat: adding style into login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,75 +2,110 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { ILogin } from "../interfaces/ILogin";
 import { login } from '../services/Requests';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 export default function Login() {
-    const [formLogin, setFormLogin] = useState({ email:'', password:'' });
-    const [enable, setEnable] = useState(true);
+  const [formLogin, setFormLogin] = useState({ email: '', password: '' });
+  const [enable, setEnable] = useState(true);
 
-    const navigate = useNavigate();
+  const theme = createTheme();
 
-    useEffect(() => {
-        const MIN_PASSWORD_LENGTH = 5;
-        const regexEmail = /\S+@\S+\.\S+/;
-        const validateEmail = regexEmail.test(formLogin.email);
-        if (formLogin.password.length >= MIN_PASSWORD_LENGTH && validateEmail) {
-            setEnable(false);
-        }
-        if (formLogin.password.length <= MIN_PASSWORD_LENGTH || !validateEmail) {
-            setEnable(true);
-        }
-    }, [formLogin]);
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        try {
-            const user = await login(formLogin as ILogin);
-            console.log(user);
-            const userData = user.data;
-            localStorage.setItem('user', JSON.stringify(userData));
-            navigate('/social-posts')
-        } catch (err) {
-            console.log(err);
-        }
-    };    
+  useEffect(() => {
+    const MIN_PASSWORD_LENGTH = 5;
+    const regexEmail = /\S+@\S+\.\S+/;
+    const validateEmail = regexEmail.test(formLogin.email);
+    if (formLogin.password.length >= MIN_PASSWORD_LENGTH && validateEmail) {
+      setEnable(false);
+    }
+    if (formLogin.password.length <= MIN_PASSWORD_LENGTH || !validateEmail) {
+      setEnable(true);
+    }
+  }, [formLogin]);
 
-    return (
-        <div>
-        <h1>Login</h1>
-        <form>
-            <label>
-                Email:
-                <input
-                placeholder="Email"
-                type="email" 
-                name="email"
-                value={ formLogin.email }
-                onChange={ ({ target }) => setFormLogin({ ...formLogin, email: target.value }) }
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                placeholder="******"
-                type="password"
-                name="password"
-                value={ formLogin.password }
-                onChange={ ({ target }) => setFormLogin({ ...formLogin, password: target.value }) }
-                />
-            </label>
-            <button
-            disabled={ enable }
-            type="button"
-            onClick={ handleLogin }
+  const handleLogin = async () => {
+    try {
+      const user = await login(formLogin as ILogin);
+      console.log(user);
+      const userData = user.data;
+      localStorage.setItem('user', JSON.stringify(userData));
+      navigate('/social-posts')
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h2">Login</Typography>
+          <Box component="form"
+            noValidate sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              autoComplete="email"
+              autoFocus
+              value={formLogin.email}
+              onChange={({ target }) => setFormLogin({ ...formLogin, email: target.value })}
+            />
+            <TextField
+              value={formLogin.password}
+              onChange={({ target }) => setFormLogin({ ...formLogin, password: target.value })}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password (min 6 characters)"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-evenly' }}
             >
+              <Button
+                disabled={enable}
+                type="button"
+                onClick={handleLogin}
+                variant="contained"
+                sx={{ mt: 3, mb: 2, width: '100px' }}
+              >
                 Login
-            </button>
-            <button
-            type="button"
-            onClick={ () => navigate('/register')}
-            >
+              </Button>
+              <Button
+                type="button"
+                onClick={() => navigate('/register')}
+                variant="contained"
+                sx={{ mt: 3, mb: 2, width: '100px' }}
+              >
                 Registrar
-            </button>
-        </form>
-        </div>
-    );
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
